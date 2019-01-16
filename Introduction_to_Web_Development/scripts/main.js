fae317c553a00a9e6cfcde64569994bc1b8e242e
Satish Chirra <Chirra.s@husky.neu.edu>
2019-01-16 15:26:30 -0500

    Add files via upload

---
+++ Introduction_to_Web_Development/scripts/main.js
@@ -0,0 +1,44 @@
+function changeImage(){
+
+	var myImage = document.querySelector('img');
+	myImage.onclick = function() {
+    	var mySrc = myImage.getAttribute('src');
+    	if(mySrc === 'images/Viz2.png') {
+      		myImage.setAttribute ('src','images/Viz3.gif');
+    	} 
+    	else {
+      		myImage.setAttribute ('src','images/Viz2.png');
+    	}
+	}
+
+}
+
+
+var myButton = document.querySelector('button');
+var myHeading = document.querySelector('h1');
+
+
+function clickButton() {
+  var user = prompt("Enter here to change username");
+  if (user != null) {
+  	myHeading.innerHTML = 'Data Visualization, ' + user;
+  }
+  else{
+  	myHeading.innerHTML = 'Data Visualization, Satish';
+  }
+
+}
+
+
+function vizTool() {
+  var tool = prompt("Please enter your favorite tool");
+  if (tool != null) {
+  	document.getElementById("viz").innerHTML = tool + " is my favorite tool!";
+  }
+  else{
+  	document.getElementById("viz").innerHTML = 'D3 is my favorite tool!';
+  }
+
+}
+
+
